f9a363f92ded3a988c41279fd9dfb7edbd138bcc
ANKUR <ankur.sh2011@gmail.com>
2023-02-10 08:36:31 +0530

    all commit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const  app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
-app.get('/',function(req,res){
-    res.send(port)
-})
+app.use('/', require('./routes'));
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+  console.log(`Server is running on port: ${port}`);
+});
