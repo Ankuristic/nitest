f201be7b22d3e405c39ecf482ce2c20eaed969d7
ANKUR <ankur.sh2011@gmail.com>
2023-02-14 12:39:10 +0530

    stylesheet added

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine','ejs');
 // app.set('views','./views');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'public')))
 // db();
 
 
